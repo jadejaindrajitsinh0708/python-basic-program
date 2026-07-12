df4e1bf4c175dfe8d4289259bd7adc7458d251d1
indrajit <jadejaindrajitsinh0708@gmail.com>
2026-07-12 19:55:47 +0530

    my project

--- test.py
+++ test.py
@@ -42,10 +42,15 @@
 
 
 
-# student(marks)
+# # student(marks)
 
 # english = float(input("enter your english marks: "))
 # gujrati = float(input("enter your gujrati marks: "))
+# match = float(input("enter your matha marks"))
+# scince = float(input("enter your scince marks"))
+# hindi = float(input("enter your hindi marks"))
+# social= float(input("enter your social marks"))
+
 # economics = float(input("enter your economics marks: "))
 # accountancy = float(input("enter your accountancy marks: "))
 # statistics = float(input("enter your statistics marks: "))
@@ -53,13 +58,13 @@
 # business_studies = float(input("enter your business studies marks: "))
 
 
-# total_marks = english + gujrati + economics + accountancy + statistics + spcc + business_studies
-# percentage = (total_marks / 700) * 100
+# total_marks = english + gujrati + match+ scince + hindi + social
+# percentage = (total_marks / 600) * 100
 
 # print(f" (Total Marks): {total_marks}")
 # print(f" (Percentage): {percentage:.2f}%") 
 
-# if english >= 33 and gujrati >= 33 and economics >= 33 and accountancy >=33 and statistics >= 33 and spcc >= 33 and business_studies >= 33 :  
+# if english >= 26 and gujrati >= 26 and match >=26 and scince >=26 and hindi >=26 and social >=26 :
 #     print("you are passs")
 # else :
 #     print("faill")
@@ -87,42 +92,128 @@
 
 
 
-balance = 10000.0
-correct_pin = 1234
+# balance = 10000.0
+# correct_pin = 1234
 
 
-while True:
-    print("\n--- ATM MENU ---")
-    print("1. Withdraw Money 💱 ")
-    print("2. Check Balance 🔄️")
-    print("3. Exit 🆗")
+# while True:
+#     print("\n--- ATM MENU ---")
+#     print("1. Withdraw Money 💱 ")
+#     print("2. Check Balance 🔄️")
+#     print("3. Exit 🆗")
     
-    choice = input("Select an option (1-3): ")
+#     choice = input("Select an option (1-3): ")
     
-    if choice == "1":
-        pay = float(input("Enter amount to withdraw: "))
-        pin = int(input("Enter your PIN: "))
+#     if choice == "1":
+#         pay = float(input("Enter amount to withdraw: "))
+#         pin = int(input("Enter your PIN: "))
         
-        if pin == correct_pin:
-            if pay <= balance:
-                balance = balance - pay  
-                print("✅ Transaction successful!")
-                print(f"Your new balance is: {balance}")
-            else:
-                print("❌ Transaction failed: Insufficient balance!")
-        else:
-            print("❌ Transaction failed: Incorrect PIN!")
+#         if pin == correct_pin:
+#             if pay <= balance:
+#                 balance = balance - pay  
+#                 print("✅ Transaction successful!")
+#                 print(f"Your new balance is: {balance}")
+#             else:
+#                 print("❌ Transaction failed: Insufficient balance!")
+#         else:
+#             print("❌ Transaction failed: Incorrect PIN!")
             
-    elif choice == "2":
-        print(f"💰 Your current balance is: {balance}")
+#     elif choice == "2":
+#         print(f"💰 Your current balance is: {balance}")
         
-    elif choice == "3":
-        print("Thank you for using our service. Goodbye! 😊")
-        break  
+#     elif choice == "3":
+#         print("Thank you for using our service. Goodbye! 😊")
+#         break  
         
-    else:
-        print("Invalid choice! Please select 1, 2, or 3.")
+#     else:
+#         print("Invalid choice! Please select 1, 2, or 3.")
 
+
+
+
+
+
+
+# num = 50
+
+# for i in range(3):
+
+#     guess = input("Guess the number between 1 to 100: ").strip()
+
+#     if not guess.isdigit():
+#         print("Enter only numbers")
+#         continue
+
+#     guess = int(guess)
+
+#     if guess == num:
+#         print("You Win")
+#         break
+#     elif guess < num:
+#         print("Your guess is low")
+#     else:
+#         print("Your guess is high")
+
+# num = 50
+
+# num2 = 0
+# while num2<3 :
+#     guess = input("enter any number :-")
+#     num2 =+1
+#     if guess.isdigit():
+#         guess=int(guess)  
+#     else :
+#         print("entre only digit")
+#         continue
+
+#     if guess == num :
+#         print("cong...")
+#         break
+#     elif guess < num :
+#         print("too high")
+#     else :
+#         print("too low")
+# else :
+#     print("game over")
+
+
+my_list = [10,20,30,40,50,60]
+def add_num(my_list):
+    
+    num = int(input("enter any number :--"))
+    my_list.append(num)
+    print(my_list)
+
+
+    # add_num(my_list)
+def remove_num(my_list):
+  
+ num = int(input("enter any number "))
+if num == my_list :
+        print("pass")
+my_list.remove(num)
+
+print(my_list)
+    # remove_num(my_list)
+while True:
+    print("option 1. number add in this list :>>")
+    print("option 2. remove number in this lost :>>")
+    print("option 3. Exit you >...")
+    if num :=input("enter your choose number :>>"):
+        if num  =="1" :
+            add_num(my_list)
+
+        elif num == "2":
+            remove_num(my_list)
+
+        elif num =="3" :
+            print("you EXIT .....")
+            break
+
+        else : 
+            print("invalid this number" )
+    # else:
+    #     print("number is not difind")
 
 
 
---
+++ test2.py
@@ -0,0 +1,212 @@
+# 1
+# 1.1
+def add_number(num):
+    num = input("enter number :-")
+    if num.isdigit():
+        num=float(num)
+
+        if num ==0:
+            print(" neutrul number ")
+        elif num >=0:
+            print("number is positive ")
+        elif num <= 0:
+            print("number is negitive ")
+    else :
+        print("invailid number")
+# add_number(num)
+
+# 1.2
+
+def odd_number(num):
+    num = input("enter number :-")
+    if num.isdigit():
+        num=int(num)
+
+        if num == 0:
+            print("numbner is neutural")
+
+        elif num %2 ==0 :
+            print("number is even ,num")
+        else :
+            print("number is odd")
+    else:
+        print("invailid number 🔄️")
+# odd_number(num)
+
+# 1.3
+def lar_number():
+    a= input("1.enter a number :")
+    b= input("2.enter a number :")
+    if a.isdigit():
+        a=int(a)
+    if b.isdigit():
+        b=int(b)
+        if a==b :
+            print("Equal numbers")
+    
+        elif a>b :
+            print("a,number is big")
+        else :
+            print("b,number is big")
+    else:
+        print("invailid number:")
+# lar_number()
+
+
+
+# 1.4
+def three_number(a,b,c):
+    
+    a = input("enter first number :")
+    b = input("enter second number :")
+    c = input("enter third number :")
+
+    if a.isdigit():
+        a=int(a)
+    if b.isdigit():
+        b=int(b)
+    if c.isdigit():
+        c=int(c)
+        if a ==b ==c :
+            print(" equal number ")
+
+        elif a >= b and a >= c :
+            print("largest number is a")
+        elif b >= a and b>=c :
+            print("largest number is ,b")
+        else :
+            print("largest number is ,c ")
+
+# three_number(a,b,c)
+
+# 5. Check voting eligibility (age ≥ 18). 
+
+def age_num(age):
+    
+    age = input("enter your age :-")
+    if age.isdigit():
+        age=float(age)
+        if age >=18 :
+            print("You are eligible for vote ✅")
+        else :
+            print("you are not eligible for vote ❌ ")
+    else :
+        print("Invalid input")
+
+# age_num(age)
+
+# 1.6
+
+def year_number():
+        year = input("Enter a year: ")
+        if year.isdigit():
+            year=int(year)
+
+            if year % 4 == 0:
+                print("Leap Year")
+            else:
+                print("Not a Leap Year")
+        else :
+            print("invalid input ")
+            print("please try agen")
+# year_number()
+
+# 1.7
+
+
+
+def student():
+    marks = input("enter your marks")
+    if marks.isdigit():
+        marks=int(marks)
+
+    
+        if marks > 100 or marks < 0:
+            print("INVALID MARKS! Please enter marks between 0 and 100.")
+
+        elif marks > 91 :
+            print("congratulation 🎉🎊 GRADE : A1")
+        elif marks == 91 :
+            print("congratulation 🎉🎊 GRADE : A1 ")
+        elif marks > 81 :
+            print("congratulation 🎉🎊 GRADE : A2 " )
+        elif marks == 81 :
+            print("congratulation 🎉🎊 GRADE : A2 ")
+        elif marks > 71 :
+            print("congratulation 🎉🎊 GRADE : B1 ")
+        elif marks == 71 :
+            print("congratulation 🎉🎊 GRADE : B1 ")
+        elif marks >61 :
+            print("congratulation 🎉🎊 GRADE : B2 ")
+        elif marks==61 :
+            print("congratulation 🎉🎊 GRADE :B2 ")
+        elif marks>51 :
+            print("congratulation 🎉🎊 GRADE : C1 ")
+        elif marks == 51 :
+            print("congratulation 🎉🎊 GRADE :C1 ")
+        elif marks> 41 :
+            print("congratulation 🎉🎊 GRADE :C2 ")
+        elif marks == 41 :
+            print("congratulation 🎉🎊 GRADE :C2 ")
+        elif marks>33:
+            print("congratulation 🎉🎊 GRADE :  D")
+        elif marks == 33 :
+            print("congratulation 🎉🎊 GRADE :D2 ")
+        else :
+           print(" FAILL")
+    else :
+        print("invalid input ")
+# student()
+# 1.8
+
+def alphabet():
+    ch = input("Enter an alphabet: ")
+
+    if ch.isalpha():
+            ch=str(ch)
+            if ch =="a" or ch =="e" and  ch=="i"and ch == "o" and ch =="u" and ch == "A" and ch == "E" or ch == "I" or ch=="O" or ch=="U" :
+                print("It is a Vowel")
+            else :
+                print("It is a Consonant")
+            
+    else :
+        print("invalid input ")
+# alphabet()
+
+# 1.9
+def calculator():
+
+    a= input("enter a first number :")
+    b = input("enter second number :")
+    if a.isdigit():
+        a=int(a)
+    if b.isdigit():
+        b=int(b)
+        
+        op = input("Operation પસંદ કરો (+, -, *, /): ")
+
+        if op=="+":
+            print(a+b)
+        elif op == "-":
+            print(a-b)
+        elif op == "*":
+            print(a*b)
+        elif op == "/":
+            print(a+b)
+        else :
+            print("INVALID INPUT ")
+    else :
+        print("INVALID NUMBER")
+
+# calculator()
+# 1.10
+def divisible_number():
+    num=int(input("enter any number :"))
+    
+    if num % 5 == 0 and num % 11 == 0:
+            print("number is divisible")
+            
+    else:
+            print("number is not divisible")
+   
+divisible_number()
